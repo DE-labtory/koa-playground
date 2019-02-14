11c0439cf7ad7674018fdf76c8a180561d8a37bc
giho.kwon <giho.kwon@navercorp.com>
2019-02-14 21:37:20 +0900

    [koa-playground] implement mock server
     - DateTime field added in DeployRequest struct
     - implement below
       - http://URL/compile
       - http://URL/deploy
       - http://URL/execute

--- backend/handlers/compile.go
+++ backend/handlers/compile.go
@@ -15,3 +15,42 @@
  */
 
 package handlers
+
+import (
+	"github.com/labstack/echo"
+	"net/http"
+	"github.com/DE-labtory/koa-playground/backend/bindings"
+	"github.com/DE-labtory/koa-playground/backend/renderings"
+	"github.com/DE-labtory/koa/abi"
+	"github.com/labstack/gommon/log"
+)
+
+var helloWorld = `contract {
+    func hello() string{
+        return "hello world!"
+    }
+}`
+
+func Compile(c echo.Context) error {
+
+	// TODO When the compiler is finished, we're going to implement all comments below!
+	// request := c.Param("CompileRequest")
+
+	request := &bindings.CompileRequest{
+		Code: helloWorld,
+	}
+
+	log.Debug(request)
+
+	// byteCodes, err :=  something.Compile(request.Code)
+	byteCodes := []byte(request.Code)
+
+	// TODO I used some random value that can use to create a value to return.
+	response := &renderings.CompileResponse{
+		ABI:         abi.ABI{},
+		RawByteCode: byteCodes,
+		ASM:         []string{request.Code},
+	}
+
+	return c.JSON(http.StatusOK, response)
+}
